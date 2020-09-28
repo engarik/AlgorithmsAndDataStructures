99ca9e858c80fb8d4cced67d6926a530fad7f591
engarik <kornik36@gmail.com>
2020-09-28 16:13:07 +0300

    Lab 2 update

--- README.md
+++ README.md
@@ -11,3 +11,11 @@
 3. [Задача C. Черепашка (2 балла)](src/Lab1/Task3.java)
 4. [Задача D. Простая сортировка (1 балл)](src/Lab1/Task4.java)
 5. [Задача E. Знакомство с жителями Сортленда (1 балл)](src/Lab1/Task5.java)
+
+### [Лабораторная работа №2](http://neerc.ifmo.ru/teaching/disalgo/problems/problems2.pdf)
+
+1. [Задача А. Сортировка (1 балл)](src/Lab2/Task1.java)
+2. [Задача B. Соревнования по бегу (1 балл)](src/Lab2/Task2.java)
+3. [Задача C. Число инверсий (2 балла)](src/Lab2/Task3.java)
+4. [Задача D. Анти-QuickSort (2 балла)](src/Lab2/Task4.java)
+5. [Задача E. K-ая порядковая статистика (2 балла)](src/Lab2/Task5.java)
---
+++ src/Lab2/Task1.java
@@ -0,0 +1,91 @@
+/* Kornilov Nikita, M3102, 25.09.2020 */
+package Lab2;
+
+import java.io.*;
+import java.util.*;
+
+public class Task1 {
+    BufferedReader br;
+    StringTokenizer in;
+    PrintWriter out;
+
+    public static void main(String[] args) {
+        new Task1().run("sort.in", "sort.out");
+    }
+
+    public String nextToken() throws IOException {
+        while (in == null || !in.hasMoreTokens())
+            in = new StringTokenizer(br.readLine());
+        return in.nextToken();
+    }
+
+    public int nextInt() throws IOException {
+        return Integer.parseInt(nextToken());
+    }
+
+    public void solve() throws IOException {
+        int n = nextInt();
+        int[] array = new int[n];
+
+        for (int i = 0; i < n; i++)
+            array[i] = nextInt();
+
+        if (isNotSorted(array))
+            quickSort(array, 0, array.length - 1);
+
+        for (int i = 0; i < array.length - 1; i++) {
+            out.print(array[i]);
+            out.print(" ");
+        }
+        out.print(array[array.length - 1]);
+    }
+
+    public void quickSort(int[] array, int leftIndex, int rightIndex) {
+        if (leftIndex < rightIndex) {
+            int pivotIndex = partition(array, leftIndex, rightIndex);
+            quickSort(array, leftIndex, pivotIndex - 1);
+            quickSort(array, pivotIndex, rightIndex);
+        }
+    }
+
+    public boolean isNotSorted(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++)
+            if (arr[i] >= arr[i + 1])
+                return true;
+        return false;
+    }
+
+    private int partition(int[] array, int leftIndex, int rightIndex) {
+        Random random = new Random();
+        int pivotIndex = array[leftIndex + random.nextInt(rightIndex - leftIndex)];
+
+        while (leftIndex <= rightIndex) {
+            while (array[leftIndex] < pivotIndex)
+                leftIndex++;
+            while (array[rightIndex] > pivotIndex)
+                rightIndex--;
+            if (leftIndex <= rightIndex) {
+                int tmp = array[leftIndex];
+                array[leftIndex] = array[rightIndex];
+                array[rightIndex] = tmp;
+                leftIndex++;
+                rightIndex--;
+            }
+        }
+        return leftIndex;
+    }
+
+    public void run(String inputFile, String outputFile) {
+        try {
+            br = new BufferedReader(new FileReader(inputFile));
+            out = new PrintWriter(outputFile);
+
+            solve();
+
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
